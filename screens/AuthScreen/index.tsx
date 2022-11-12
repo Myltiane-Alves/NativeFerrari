6df159f04e33a71513aa0d2670152115689f02b4
Myltiane <myltiane.aux@gmail.com>
2022-11-12 13:18:23 -0300

    feat: iniciando telas de autenticação

---
+++ screens/AuthScreen/index.tsx
@@ -0,0 +1,11 @@
+import { createStackNavigator } from "@react-navigation/native"
+
+const Stack = createStackNavigator<typeof Screen>()
+export const AuthScreen = (props) => {
+
+    return (
+        <Stack.Navigator>
+
+        </Stack.Navigator>
+    )
+}
