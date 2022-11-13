aa38e2ea9178c013f40053bbc7463412f1ef8ea7
Myltiane <myltiane.aux@gmail.com>
2022-11-13 14:17:45 -0300

    feat: formulario register

--- components/Auth/AuthFormEmail/index.tsx
+++ components/Auth/AuthFormEmail/index.tsx
@@ -1,3 +1,5 @@
+import { useDrawerNavigation } from "../../../hooks/useDrawerNavigation"
+import { Screen } from "../../../screens"
 import { vars } from "../../../values"
 import { Button } from "../../Button"
 import { InputField } from "../../InputField"
@@ -6,12 +8,17 @@ import { AuthFormFooter } from "../AuthFormFooter"
 import { AuthFormEmailWrap } from "./AuthFormEmailWrap"
 
 export const AuthFormEmail = () => {
+    const navigation = useDrawerNavigation();
     return (
         <AuthFormEmailWrap>
             <PageTitle title="Autenticação" />
             <InputField label="E-mail" style={{ marginTop: vars.space}}/>
             <AuthFormFooter>
-                <Button color="text">Criar uma conta</Button>
+                <Button 
+                    color="text"
+                    onPress={() => navigation.navigate(Screen.AuthRegister)} 
+                >   
+                    Criar uma conta</Button>
                 <Button color="green">Próximo</Button>
             </AuthFormFooter>
         </AuthFormEmailWrap>
